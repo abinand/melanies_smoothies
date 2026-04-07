d596f62a1070fc7a960e0cb2ef5d0301638c6c4a
Abinand Sivakumar <abinand.as@gmail.com>
2026-04-07 17:34:10 -0400

    Comment out dataframe display and stop in streamlit_app
    Comment out dataframe display and stop execution for debugging.

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 ## Convert Snowpark Dataframe to a Pandas Dataframe to use loc for lookup
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
@@ -36,13 +36,12 @@ if ingredients_list:
 
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-        
-        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-      
-        st.subheader(fruit_chosen + 'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{search_on}")
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+      ingredients_string += fruit_chosen + ' '
+      search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+      st.write('Debug: search value for ', fruit_chosen, ' is ', search_on, '.')
+      st.subheader(fruit_chosen + ' Nutrition Information')
+      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{search_on}")
+      sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         
     #st.write(ingredients_string)
 
